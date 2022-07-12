8e09a95bb03a30e13d471a92e2fb3c2db924ce37
iiiivanCh <iiiivanchm@yandex.ru>
2022-07-13 01:56:05 +0300

    Выполнил ДЗ

---
+++ js/index.js
@@ -0,0 +1,7 @@
+"use strict"
+
+const nameForm = document.querySelector('.top__title');
+const numberID = document.querySelector('.top__btn');
+const form = document.querySelector('form');
+const coupleDiscount = document.querySelector('.edit__couple');
+const totalProduct = document.querySelector('.modal__total-number');
